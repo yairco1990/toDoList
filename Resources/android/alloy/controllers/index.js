d0d7692152413563d7849bd6f4784d4ea02346fa
Yair Cohen <yairco1990@gmail.com>
2014-12-26 15:23:28 +0200

    Fix bugs, a few style change, added 'delete all' button and databse

--- Resources/android/alloy/controllers/index.js
+++ Resources/android/alloy/controllers/index.js
@@ -8,12 +8,84 @@ function __processArg(obj, key) {
 }
 
 function Controller() {
-    function cleanText(e) {
-        console.log(e);
-        $.textField.value = "";
+    function reloadFromDatabase(myText) {
+        var row = Ti.UI.createTableViewRow({
+            height: "80"
+        });
+        var label = Ti.UI.createLabel({
+            text: myText,
+            height: Titanium.UI.SIZE,
+            width: Titanium.UI.SIZE,
+            color: "black",
+            layout: "vertical"
+        });
+        var DoneCheckbox = Ti.UI.createButton({
+            title: "Done",
+            top: 8,
+            right: 10,
+            width: 70,
+            height: 30,
+            borderColor: "#666",
+            borderWidth: 2,
+            borderRadius: 3,
+            backgroundColor: "green",
+            backgroundImage: "none",
+            color: "#fff",
+            font: {
+                fontSize: 23,
+                fontWeight: "bold"
+            }
+        });
+        var DeleteCheckbox = Ti.UI.createButton({
+            id: "delrow",
+            myrow: row,
+            mylabel: label,
+            title: "Delete",
+            bottom: 8,
+            right: 10,
+            width: 70,
+            height: 30,
+            borderColor: "#666",
+            borderWidth: 2,
+            borderRadius: 3,
+            backgroundColor: "red",
+            backgroundImage: "none",
+            color: "#fff",
+            font: {
+                fontSize: 19,
+                fontWeight: "bold"
+            }
+        });
+        var imageShow = "/appicon.png";
+        "/Vdishes.png" == $.dishes.image ? imageShow = "/dishes.png" : "/Vlaundery.png" == $.laundery.image && (imageShow = "/laundery.png");
+        var image = Ti.UI.createView({
+            backgroundImage: imageShow,
+            width: "70",
+            height: "70",
+            left: "5"
+        });
+        row.add(DoneCheckbox);
+        row.add(DeleteCheckbox);
+        row.add(image);
+        row.add(label);
+        $.tableView.appendRow(row);
+        DoneCheckbox.addEventListener("click", function() {
+            row.backgroundColor = "#abce12";
+            var lineView = Titanium.UI.createView({
+                width: label.getSize().width,
+                height: 1,
+                backgroundColor: label.color ? label.color : "black"
+            });
+            row.add(lineView);
+        });
+    }
+    function cleanText() {
+        if (false == alreadyClicked) {
+            $.textField.value = "";
+            alreadyClicked = true;
+        }
     }
     function doClick(e) {
-        console.log(e);
         if ("dishes" == e.source.id) {
             "/Vdishes.png" != e.source.image && (e.source.image = "/Vdishes.png");
             "/laundery.png" != $.laundery.image && ($.laundery.image = "/laundery.png");
@@ -44,78 +116,109 @@ function Controller() {
         id: "win"
     });
     $.__views.win && $.addTopLevelView($.__views.win);
-    $.__views.headerView = Ti.UI.createView({
-        top: "50",
-        height: "Ti.UI.SIZE",
-        id: "headerView"
+    $.__views.__alloyId0 = Ti.UI.createView({
+        height: "60",
+        backgroundColor: "#848484",
+        id: "__alloyId0"
     });
-    $.__views.win.add($.__views.headerView);
+    $.__views.win.add($.__views.__alloyId0);
     $.__views.textField = Ti.UI.createTextField({
         borderStyle: Ti.UI.INPUT_BORDERSTYLE_ROUNDED,
-        top: "13",
+        top: "3",
         left: "10",
-        right: "300",
-        value: "Enter your task.",
+        right: "100",
+        value: "Enter your task",
         textAlign: "left",
-        height: "65",
+        height: "55",
         font: {
-            fontSize: 50,
+            fontSize: 27,
             fontWeight: "bold"
         },
         id: "textField"
     });
-    $.__views.headerView.add($.__views.textField);
+    $.__views.__alloyId0.add($.__views.textField);
     cleanText ? $.__views.textField.addEventListener("click", cleanText) : __defers["$.__views.textField!click!cleanText"] = true;
     $.__views.addButton = Ti.UI.createButton({
         right: "10",
-        top: "10",
-        height: "70",
+        top: "3",
+        height: "55",
         width: "80",
         title: "Add!",
         id: "addButton"
     });
-    $.__views.headerView.add($.__views.addButton);
+    $.__views.__alloyId0.add($.__views.addButton);
+    $.__views.ChooseImage = Ti.UI.createView({
+        backgroundColor: "#848484",
+        height: "75",
+        id: "ChooseImage"
+    });
+    $.__views.win.add($.__views.ChooseImage);
     $.__views.dishes = Ti.UI.createImageView({
         image: "/dishes.png",
         width: "70",
         height: "70",
         right: "110",
-        top: "10",
+        top: "3",
         id: "dishes"
     });
-    $.__views.headerView.add($.__views.dishes);
+    $.__views.ChooseImage.add($.__views.dishes);
     doClick ? $.__views.dishes.addEventListener("click", doClick) : __defers["$.__views.dishes!click!doClick"] = true;
     $.__views.laundery = Ti.UI.createImageView({
         image: "/laundery.png",
         width: "70",
         height: "70",
         right: "200",
-        top: "10",
+        top: "3",
         id: "laundery"
     });
-    $.__views.headerView.add($.__views.laundery);
+    $.__views.ChooseImage.add($.__views.laundery);
     doClick ? $.__views.laundery.addEventListener("click", doClick) : __defers["$.__views.laundery!click!doClick"] = true;
-    $.__views.__alloyId0 = Ti.UI.createView({
+    $.__views.deleteAllButton = Ti.UI.createButton({
+        height: "50",
+        width: Titanium.UI.FILL,
+        title: "Click to delete all",
+        id: "deleteAllButton"
+    });
+    $.__views.win.add($.__views.deleteAllButton);
+    $.__views.__alloyId1 = Ti.UI.createView({
         height: "2",
         backgroundColor: "#abce12",
-        top: "5",
-        id: "__alloyId0"
+        top: "2",
+        id: "__alloyId1"
     });
-    $.__views.win.add($.__views.__alloyId0);
+    $.__views.win.add($.__views.__alloyId1);
     $.__views.tableView = Ti.UI.createTableView({
         top: "5",
         id: "tableView"
     });
     $.__views.win.add($.__views.tableView);
+    $.__views.__alloyId2 = Ti.UI.createView({
+        height: "5",
+        backgroundColor: "#848484",
+        id: "__alloyId2"
+    });
+    $.__views.win.add($.__views.__alloyId2);
     exports.destroy = function() {};
     _.extend($, $.__views);
     $.win.open();
+    var db = Titanium.Database.install("dbRowData.db", "version6");
+    db.execute("CREATE TABLE IF NOT EXISTS Tasks(id INTEGER PRIMARY KEY AUTOINCREMENT, task TEXT);");
+    var sql = db.execute("SELECT * FROM Tasks");
+    while (sql.isValidRow()) {
+        reloadFromDatabase(sql.fieldByName("task"));
+        sql.next();
+    }
+    var alreadyClicked = false;
     var addTask = function() {
         var row = Ti.UI.createTableViewRow({
             height: "80"
         });
         var label = Ti.UI.createLabel({
-            text: $.textField.getValue().fontsize(8)
+            text: $.textField.getValue(),
+            height: Titanium.UI.SIZE,
+            width: Titanium.UI.SIZE,
+            color: "black",
+            layout: "vertical"
         });
         var DoneCheckbox = Ti.UI.createButton({
             title: "Done",
@@ -137,6 +240,7 @@ function Controller() {
         var DeleteCheckbox = Ti.UI.createButton({
             id: "delrow",
             myrow: row,
+            mylabel: label,
             title: "Delete",
             bottom: 8,
             right: 10,
@@ -149,7 +253,7 @@ function Controller() {
             backgroundImage: "none",
             color: "#fff",
             font: {
-                fontSize: 22,
+                fontSize: 19,
                 fontWeight: "bold"
             }
         });
@@ -168,9 +272,34 @@ function Controller() {
         $.tableView.appendRow(row);
         DoneCheckbox.addEventListener("click", function() {
             row.backgroundColor = "#abce12";
+            var lineView = Titanium.UI.createView({
+                width: label.getSize().width,
+                height: 1,
+                backgroundColor: label.color ? label.color : "black"
+            });
+            row.add(lineView);
         });
+        var db = Titanium.Database.open("version6");
+        db.execute("INSERT INTO Tasks (task) VALUES (?)", $.textField.getValue());
+        db.close();
+        $.textField.value = "";
     };
+    $.tableView.addEventListener("click", function(event) {
+        if ("delrow" == event.source.id) {
+            $.tableView.deleteRow(event.index);
+            var db = Titanium.Database.open("version6");
+            db.execute("DELETE FROM Tasks WHERE task = (?)", event.source.mylabel.getText());
+            db.close();
+        }
+    });
     $.addButton.addEventListener("click", addTask);
+    $.deleteAllButton.addEventListener("click", function() {
+        var rd = [];
+        $.tableView.data = rd;
+        var db = Titanium.Database.open("version6");
+        db.execute("DELETE FROM Tasks");
+        db.close();
+    });
     __defers["$.__views.textField!click!cleanText"] && $.__views.textField.addEventListener("click", cleanText);
     __defers["$.__views.dishes!click!doClick"] && $.__views.dishes.addEventListener("click", doClick);
     __defers["$.__views.laundery!click!doClick"] && $.__views.laundery.addEventListener("click", doClick);
--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,31 +1,25 @@
 $.win.open();
 
-function cleanText(e) {
-	console.log(e);
-	$.textField.value = "";
+//database addition
+var db = Titanium.Database.install('\db\RowData.db', 'version6');
+db.execute('CREATE TABLE IF NOT EXISTS Tasks(id INTEGER PRIMARY KEY AUTOINCREMENT, task TEXT);');
+var sql = db.execute('SELECT * FROM Tasks');
+while (sql.isValidRow()) {
+	reloadFromDatabase(sql.fieldByName('task'));
+	sql.next();
 }
+//
 
-function doClick(e) {
-	console.log(e);
-	if (e.source.id == "dishes") {
-		if (e.source.image != '/Vdishes.png')
-			e.source.image = '/Vdishes.png';
-		if ($.laundery.image != '/laundery.png')
-			$.laundery.image = '/laundery.png';
-	} else {
-		if (e.source.image != '/Vlaundery.png')
-			e.source.image = '/Vlaundery.png';
-		if ($.dishes.image != '/dishes.png')
-			$.dishes.image = '/dishes.png';
-	}
-}
-
-var addTask = function(e) {
+function reloadFromDatabase(myText) {
 	var row = Ti.UI.createTableViewRow({
 		height : "80"
 	});
 	var label = Ti.UI.createLabel({
-		text : $.textField.getValue().fontsize(8)
+		text : myText,
+		height : Titanium.UI.SIZE,
+		width : Titanium.UI.SIZE,
+		color : "black",
+		layout : 'vertical',
 	});
 	var DoneCheckbox = Ti.UI.createButton({
 		title : 'Done',
@@ -45,8 +39,9 @@ var addTask = function(e) {
 		}
 	});
 	var DeleteCheckbox = Ti.UI.createButton({
-		id: "delrow",
-		myrow : row, // customProperty
+		id : "delrow",
+		myrow : row,
+		mylabel : label,
 		title : 'Delete',
 		bottom : 8,
 		right : 10,
@@ -59,7 +54,7 @@ var addTask = function(e) {
 		backgroundImage : 'none',
 		color : '#fff',
 		font : {
-			fontSize : 22,
+			fontSize : 19,
 			fontWeight : 'bold'
 		}
 	});
@@ -81,16 +76,132 @@ var addTask = function(e) {
 	$.tableView.appendRow(row);
 	DoneCheckbox.addEventListener("click", function(event) {
 		row.backgroundColor = "#abce12";
+		var lineView = Titanium.UI.createView({
+			width : label.getSize().width,
+			height : 1,
+			backgroundColor : label.color ? label.color : 'black',
+		});
+		row.add(lineView);
 	});
 };
 
+var alreadyClicked = false;
+function cleanText(e) {
+	if (alreadyClicked == false) {
+		$.textField.value = "";
+		alreadyClicked = true;
+	}
+}
+
+function doClick(e) {
+	if (e.source.id == "dishes") {
+		if (e.source.image != '/Vdishes.png')
+			e.source.image = '/Vdishes.png';
+		if ($.laundery.image != '/laundery.png')
+			$.laundery.image = '/laundery.png';
+	} else {
+		if (e.source.image != '/Vlaundery.png')
+			e.source.image = '/Vlaundery.png';
+		if ($.dishes.image != '/dishes.png')
+			$.dishes.image = '/dishes.png';
+	}
+}
+
+var addTask = function(e) {
+	var row = Ti.UI.createTableViewRow({
+		height : "80"
+	});
+	var label = Ti.UI.createLabel({
+		text : $.textField.getValue(),
+		height : Titanium.UI.SIZE,
+		width : Titanium.UI.SIZE,
+		color : "black",
+		layout : 'vertical',
+	});
+	var DoneCheckbox = Ti.UI.createButton({
+		title : 'Done',
+		top : 8,
+		right : 10,
+		width : 70,
+		height : 30,
+		borderColor : '#666',
+		borderWidth : 2,
+		borderRadius : 3,
+		backgroundColor : 'green',
+		backgroundImage : 'none',
+		color : '#fff',
+		font : {
+			fontSize : 23,
+			fontWeight : 'bold'
+		}
+	});
+	var DeleteCheckbox = Ti.UI.createButton({
+		id : "delrow",
+		myrow : row,
+		mylabel : label,
+		title : 'Delete',
+		bottom : 8,
+		right : 10,
+		width : 70,
+		height : 30,
+		borderColor : '#666',
+		borderWidth : 2,
+		borderRadius : 3,
+		backgroundColor : 'red',
+		backgroundImage : 'none',
+		color : '#fff',
+		font : {
+			fontSize : 19,
+			fontWeight : 'bold'
+		}
+	});
+	var imageShow = "/appicon.png";
+	if ($.dishes.image == '/Vdishes.png')
+		imageShow = "/dishes.png";
+	else if ($.laundery.image == '/Vlaundery.png')
+		imageShow = "/laundery.png";
+	var image = Ti.UI.createView({
+		backgroundImage : imageShow,
+		width : "70",
+		height : "70",
+		left : "5"
+	});
+	row.add(DoneCheckbox);
+	row.add(DeleteCheckbox);
+	row.add(image);
+	row.add(label);
+	$.tableView.appendRow(row);
+	DoneCheckbox.addEventListener("click", function(event) {
+		row.backgroundColor = "#abce12";
+		var lineView = Titanium.UI.createView({
+			width : label.getSize().width,
+			height : 1,
+			backgroundColor : label.color ? label.color : 'black',
+		});
+		row.add(lineView);
+	});
+	var db = Titanium.Database.open('version6');
+	db.execute('INSERT INTO Tasks (task) VALUES (?)', $.textField.getValue());
+	db.close();
+	$.textField.value = "";
+};
+
 $.tableView.addEventListener('click', function(event) {
-	if(event.source.id == "delrow"){
-		console.log(event);
+	if (event.source.id == "delrow") {
 		$.tableView.deleteRow(event.index);
-	}	
+		var db = Titanium.Database.open('version6');
+		db.execute('DELETE FROM Tasks WHERE task = (?)', event.source.mylabel.getText());
+		db.close();
+	}
 });
 
 $.addButton.addEventListener("click", addTask);
 
+$.deleteAllButton.addEventListener("click", function(e) {
+	var rd = [];
+	$.tableView.data = rd;
+	var db = Titanium.Database.open('version6');
+	db.execute('DELETE FROM Tasks');
+	db.close();
+});
 
